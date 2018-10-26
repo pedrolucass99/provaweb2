ee637ef6503715105ef0e805a2e1006dd3f9f947
pedrolucass99 <pedrolucastitan@hotmail.com>
2018-10-26 09:22:12 -0300

    prova

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('broken', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('sigla');
+            $table->string('endereco');
+            $table->number('numerop')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('broken');
+    }
+}
